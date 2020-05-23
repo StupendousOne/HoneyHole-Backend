aa9d82897f700635cecf34cb2b2dc4c9b9e50adb
rpdecks <rpdecks@gmail.com>
2020-05-23 13:40:36 -0400

    seed file creates users and fish (from api)

--- Gemfile
+++ Gemfile
@@ -23,11 +23,16 @@ gem 'puma', '~> 4.1'
 gem 'bootsnap', '>= 1.4.2', require: false
 
 # Use Rack CORS for handling Cross-Origin Resource Sharing (CORS), making cross-origin AJAX possible
-# gem 'rack-cors'
+gem 'rack-cors'
+#
+# Use rest client gem to pull api data for seeding
+gem 'rest-client'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
+  gem 'pry'
+  gem 'faker'
 end
 
 group :development do
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,24 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'rest-client'
+require 'pry'
+require 'faker'
+
+User.destroy_all
+
+# if we wanted to isolate certain fish, we could use these SpecCodes from the JSON data but we would have to pull thousands of records to get all of these unless we can find a way to revise our query to include these
+
+    # popular_fish_code = [3385, 58432, 3382, 62256, 66684, 3384, 239, 3387, 3388, 268, 3019, 290, 79, 1450]
+
+res = RestClient.get 'https://fishbase.ropensci.org/species', params: {limit: 50}
+# isolates key with fish data
+res_ary = JSON.parse(res)["data"]
+# filters the array to get better curated collection of fish for our app
+fish_ary = res_ary.select { |fish| fish["FBname"] != nil && fish["GameFish"] = -1 && fish["Fresh"] = -1 }
+
+20.times do
+    User.create(name: Faker::Name.name, bio: Faker::GreekPhilosophers.quote, username: Faker::Internet.username, email: Faker::Internet.email)
+end
+
+fish_ary.each do |fish|
+    Fish.create(species: fish["FBname"], description: fish["Comments"])
+end
+# binding.pry
