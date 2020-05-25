b3575d3842630dcf491db19af41a2e85a05a5040
rpdecks <rpdecks@gmail.com>
2020-05-25 10:52:40 -0400

    added image to fish needs a db migrate

--- app/controllers/api/v1/fishing_spots_controller.rb
+++ app/controllers/api/v1/fishing_spots_controller.rb
@@ -1,2 +1,47 @@
 class Api::V1::FishingSpotsController < ApplicationController
+    before_action :find_fishing_spot, only: [:show, :update, :destroy]
+
+    def index
+        @fishing_spots = FishingSpot.all
+        render :json => @fishing_spots, include: :fish, status: :ok
+    end
+
+    def create
+        @fishing_spot = FishingSpot.create(fishing_spot_params)
+        if @fishing_spot.persisted?
+            render :json => @fishing_spot, status: :created
+        else
+            render :json => { errors: @fishing_spot.errors }
+        end
+    end
+
+    def show
+        if @fishing_spot
+            render :json => @fishing_spot
+        else
+            render :json => {error: 'Fishing Spot not found'}, status: :not_found
+        end
+    end
+
+    def update
+        @fishing_spot.update(fishing_spot_params)
+        if @fishing_spot.persisted?
+            render :json => @fishing_spot, status: :ok
+        else
+            render :json => { errors: @fishing_spot.errors }
+        end
+    end
+
+    def destroy
+        render :json => { :message => "Succesful." }
+    end
+
+    private
+    def fishing_spot_params
+        params.require(:fishing_spot).permit(:name,:image,:public_access,:site_info,:latitude,:longitude,:user_id)
+    end
+
+    def find_fishing_spot
+        @fishing_spot = FishingSpot.find_by(id: params[:id])
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ApplicationRecord
     has_many :fishing_spots, dependent: :destroy
     has_many :reviews, dependent: :destroy
     has_many :favorites, dependent: :destroy
-    has_many :favorite_fishing_spots, through: :favorites, dependent: :destroy
-    has_many :reviewed_fishing_spots, through: :reviews, dependent: :destroy
+    has_many :favorite_fishing_spots, through: :favorites
+    has_many :reviewed_fishing_spots, through: :reviews
 end
