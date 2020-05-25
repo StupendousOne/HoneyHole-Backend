4008ef2db4742c4cf69e83625f720ff2af8c13ff
rpdecks <rpdecks@gmail.com>
2020-05-25 10:38:42 -0400

    fish images added to table and seed. db migrate to see

--- db/seeds.rb
+++ db/seeds.rb
@@ -27,7 +27,7 @@ res_ary = JSON.parse(res)["data"]
 fish_ary = res_ary.select { |fish| fish["FBname"] != nil && fish["GameFish"] = -1 && fish["Fresh"] = -1 }
 # create seed data
 fish_ary.each do |fish|
-    Fish.create(species: fish["FBname"], description: fish["Comments"])
+    Fish.create(species: fish["FBname"], description: fish["Comments"], image: fish["image"])
 end
 
 #FISHING SPOT API endpoint
