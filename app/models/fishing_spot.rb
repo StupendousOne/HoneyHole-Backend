5a95c14ef6117f7e99ab4bb23794a49f81679b69
StupendousOne <harrisongeo87@gmail.com>
2020-05-25 09:58:09 -0400

    fishing spot controller

--- app/models/fishing_spot.rb
+++ app/models/fishing_spot.rb
@@ -2,7 +2,7 @@ class FishingSpot < ApplicationRecord
   belongs_to :user
   has_many :reviews, dependent: :destroy
   has_many :favorites, dependent: :destroy
-  has_many :users, through: :reviews, dependent: :destroy
+  has_many :users, through: :reviews
   has_many :fish_spots, dependent: :destroy
-  has_many :fish, through: :fish_spots, dependent: :destroy
+  has_many :fish, through: :fish_spots
 end
