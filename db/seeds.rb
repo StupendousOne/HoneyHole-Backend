0a7df36d693da9f47a2b78966c2658c096f91adb
rpdecks <rpdecks@gmail.com>
2020-05-26 14:57:17 -0400

    updates to seed file and validation for fish_spots

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,22 +15,6 @@ FishSpot.destroy_all
     User.create(name: Faker::Name.name, bio: Faker::GreekPhilosophers.quote, username: Faker::Internet.username, email: Faker::Internet.email, is_active: true)
 end
 
-#FISH API
-# if we wanted to isolate certain fish, we could use these SpecCodes from the JSON data but we would have to pull thousands of records to get all of these unless we can find a way to revise our query to include these
-    # popular_fish_code = [3385, 58432, 3382, 62256, 66684, 3384, 239, 3387, 3388, 268, 3019, 290, 79, 1450]
-
-FISH_url = 'https://fishbase.ropensci.org/species'
-res = RestClient.get FISH_url, params: {limit: 50}
-# isolates key with fish data
-res_ary = JSON.parse(res)["data"]
-# filters the array to get better curated collection of fish for our app
-fish_ary = res_ary.select { |fish| fish["FBname"] != nil && fish["GameFish"] = -1 && fish["Fresh"] = -1 }
-# create seed data
-fish_ary.each do |fish|
-    Fish.create(species: fish["FBname"], description: fish["Comments"], image: fish["image"], is_active: true)
-end
-
-
 #FISHING SPOT API endpoint
 SPOT_url = "https://data.ny.gov/resource/f4vj-p8y5.json"
 # have to set up account to get app token and thereby permission to ping/request
@@ -44,7 +28,30 @@ spots = response.body
 # iterate over spots to create new records with API data
 # TODO needs real lake/river images to replace Faker Avatar images
 spots.each do |spot|
-    FishingSpot.create(user_id: User.all.sample.id, name: spot.name, longitude: spot.location.longitude, latitude: spot.location.latitude, site_info: spot.site_wl, public_access: spot.public_acc, image: Faker::Avatar.image, is_active: true)
+    fs = FishingSpot.new(user_id: User.all.sample.id, name: spot.name, longitude: spot.location.longitude, latitude: spot.location.latitude, public_access: spot.public_acc, image: Faker::Avatar.image, is_active: true)
+    if !spot.site_wl
+        fs.save!
+    else
+        spot.site_wl.url
+        fs.site_info = spot.site_wl.url
+        fs.save!
+    end
+    spot_ary.push(fs)
+end
+
+#FISH API
+# if we wanted to isolate certain fish, we could use these SpecCodes from the JSON data but we would have to pull thousands of records to get all of these unless we can find a way to revise our query to include these
+    # popular_fish_code = [3385, 58432, 3382, 62256, 66684, 3384, 239, 3387, 3388, 268, 3019, 290, 79, 1450]
+
+FISH_url = 'https://fishbase.ropensci.org/species'
+res = RestClient.get FISH_url, params: {limit: 50}
+# isolates key with fish data
+res_ary = JSON.parse(res)["data"]
+# filters the array to get better curated collection of fish for our app
+fish_ary = res_ary.select { |fish| fish["FBname"] != nil && fish["GameFish"] = -1 && fish["Fresh"] = -1 }
+# create seed data
+fish_ary.each do |fish|
+    Fish.create(species: fish["FBname"], description: fish["Comments"], image: fish["image"], is_active: true)
 end
 
 # create reviews seed data
